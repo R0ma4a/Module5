2b4718beb4d080bb527b22aeb00832152f8d531d
Roman <roman.gergun@gmail.com>
2025-08-09 14:11:00 +0800

    first task done

---
+++ task1.js
@@ -0,0 +1,11 @@
+var name = "Roman";
+
+var age = 21;
+
+var isStudent = true;
+
+console.log("The name variable has the value " + name + " and is of type " + typeof name + "\n");
+
+console.log("The age variable has the value " + age + " and is of type " + typeof age + "\n");
+
+console.log("The isStudent variable has the value " + isStudent + " and is of type " + typeof isStudent);
