d61fbdc0bf5b49611564a96af686372d6c9435cc
Roman <roman.gergun@gmail.com>
2025-08-09 14:52:34 +0800

    task 4 done

---
+++ task4.js
@@ -0,0 +1,11 @@
+function calculateArea(length, width){
+    return length * width;
+}
+
+console.log("The area of a 5x4 rectangle is: " + calculateArea(5, 4));
+
+console.log("The area of a 1x1 rectangle is: " + calculateArea(1, 1));
+
+console.log("The area of a 6x6 rectangle is: " + calculateArea(6, 6));
+
+console.log("The area of a 7.5x4.3 rectangle is: " + calculateArea(7.5, 4.3));
